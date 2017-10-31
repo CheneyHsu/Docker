a3e6bd47c736e09ab044db18a270ec672679794e
CheneyHsu <qqbxclboy@126.com>
2017-10-31 10:51:53 +0800

    add

---
+++ Wirite-Docker/mydocker/main.go
@@ -0,0 +1,12 @@
+package main
+import (
+log "github.com/Sirupsen/logrus"
+"github.com/urfave/cli"
+"os"
+)
+
+const usage = `mydocker is a simple container runtime implementation.`
+
+func main() {
+	app := cli.NewApp()
+}
